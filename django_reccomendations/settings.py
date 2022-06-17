9f3025ac9a3d7607afe906f6e8d119a2acf1e1b1
SKY3E <91026687+SKY3E@users.noreply.github.com>
2022-06-16 23:37:53 -0400

    Update settings.py

--- django_reccomendations/settings.py
+++ django_reccomendations/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h4euuz=i3j^5d%9c)mo^st*&1#p@)&1*)7u@9l$!6o0k$&g))5'
+SECRET_KEY = '[Placeholder]'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
